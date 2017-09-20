9496c45d5d9f6de25e80f7b61dc572a014a362b9
rafaelBadan <rafael.badan@gmail.com>
2017-09-20 20:09:13 -0300

    created a currency_list.json and first test as convert_spec.rb

---
+++ spec/lib/convert_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require './lib/convert'
+
+describe Currency do
+  it "convert" do
+    json = JSON.parse(File.read("./spec/fixtures/currency_list.json"))
+
+    # variable currency is loaded with the first currency sampled from currency_list.json
+    # variable currency_destination is loaded with the second sampled currency from currency_list.json
+    currency, currency_destination = json['currency_list'].sample(2)
+
+    #The amount of money to be converted
+    amount = rand(1..9999)
+
+    value = Currency::convert currency, currency_destination, amount
+    expect(value.is_a? Numeric and value != 0).to eql(true)
+  end
+end

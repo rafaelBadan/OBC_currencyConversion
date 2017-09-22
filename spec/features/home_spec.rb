643a9dc75b24118070e5b37fc7686bb2b8cc3751
rafaelBadan <rafael.badan@gmail.com>
2017-09-22 17:13:25 -0300

    created and styled home view

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "convert Currency Process", :type => :feature do
+  it "convert value" do
+    visit '/'
+    within("#convert_form") do
+      select('EUR', from: 'currency')
+      select('USD', from: 'currency_destination')
+      fill_in 'amount', with: '10'
+    end
+    click_button 'CONVERT'
+
+    save_and_open_page
+    expect(page).to have_content("value")
+  end
+end
